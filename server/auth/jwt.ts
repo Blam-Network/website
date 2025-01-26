36536dfc9667bab34d4ce725e6e1d36235765061
Codie Newark <a13x231mcsl@gmail.com>
2025-01-26 03:07:24 +0000

    handle missing refresh token i guess

--- server/auth/jwt.ts
+++ server/auth/jwt.ts
@@ -15,7 +15,7 @@ const JWTSchema = z.object({
         xsts: z.string(),
     }),
     accessToken: z.string(),
-    refreshToken: z.string(),
+    refreshToken: z.string().optional(),
 });
 
 export type SunriseJWT = z.infer<typeof JWTSchema>;
@@ -24,6 +24,7 @@ export const getParsedToken = async (
     ...params: Parameters<typeof getToken>
   ): Promise<SunriseJWT | undefined> => {
     const token = await getToken(...params);
+    if (token === null) return undefined;
     const parsed = JWTSchema.safeParse(token);
     if (!parsed.success) throw new Error(JSON.stringify(parsed.error));
     if (!parsed.success) return undefined;
