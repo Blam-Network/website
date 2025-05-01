aa4348850c2d784b4229fd204ed0579424140e17
Codie Newark <a13x231mcsl@gmail.com>
2025-05-01 23:29:51 +0100

    Missed from commit

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getServerSession } from "next-auth";
+import { getSession, SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 const queryClient = new QueryClient()
 
 export const Providers = ({ children }: { children: ReactNode}) => {
     return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
+        <SessionProvider>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </SessionProvider>
     );
 }
