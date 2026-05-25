344f90e8fb015a6596a17079246e2cb79ec2ba8e
Codie Newark <git@codie.gg>
2026-05-25 22:53:25 +0900

    remove swc

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,38 +1,7 @@
-import swc from "unplugin-swc";
 import { defineConfig } from "vitest/config";
 
-const swcDecorators = {
-  legacyDecorator: false,
-  decoratorVersion: "2022-03" as const,
-  decoratorMetadata: false,
-  useDefineForClassFields: true,
-};
-
 export default defineConfig({
-  plugins: [
-    swc.vite({
-      jsc: {
-        parser: {
-          syntax: "typescript",
-          decorators: true,
-        },
-        transform: swcDecorators,
-        target: "es2022",
-      },
-    }),
-  ],
   test: {
     include: ["src/**/*.test.ts"],
-    deps: {
-      optimizer: {
-        swc: {
-          jsc: {
-            parser: { syntax: "typescript", decorators: true },
-            transform: swcDecorators,
-            target: "es2022",
-          },
-        },
-      },
-    },
   },
 });
