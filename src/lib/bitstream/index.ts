2892d6952f02a8c4f5e30e84e30294fc37444e94
Codie Newark <git@codie.gg>
2026-05-25 22:49:26 +0900

    remove lib folder???

--- src/lib/bitstream/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  e_bitstream_byte_order,
-  e_bitstream_byte_fill_direction,
-  e_bitstream_state,
-} from "./enums";
-export { BitstreamError, assert_ok } from "./errors";
-export * from "./math";
-export { c_bitstream_reader } from "./reader";
-export { c_bitstream_writer } from "./writer";
